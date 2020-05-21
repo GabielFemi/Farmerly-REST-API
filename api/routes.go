5944cd9f59113008237e0345cf6b1ad46150e5c7
GabielFemi <gabrielfemi799@gmail.com>
2020-05-21 13:39:20 +0200

    set response and also encode a dummy text to the response

--- api/routes.go
+++ api/routes.go
@@ -1,9 +1,13 @@
 package api
 
 import (
+	"encoding/json"
 	"net/http"
 )
 
 func Index(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(200)
+	_ = json.NewEncoder(w).Encode("welcome: gabriel")
+
 }
